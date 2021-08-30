b98ae32ed30ecfbbd0b2d4e2592c613b71f69ba4
shegga9x <74436981+shegga9x@users.noreply.github.com>
2021-08-30 08:24:24 +0700

    update again

---
+++ backend/Controllers/MangaController.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using backend.Data;
+using backend.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class MangaController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public MangaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Manga
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Manga>>> GetManga()
+        {
+
+            return await _context.Manga.ToListAsync();
+        }
+        // GET: api/Manga/5
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Manga>> GetManga(string id)
+        {
+            var manga = await _context.Manga.Where(b => b.id == id).Include(b => b.detail).Include(b => b.chaps).FirstOrDefaultAsync();
+            if (manga == null)
+            {
+                return NotFound();
+            }
+
+            return manga;
+        }
+
+        // PUT: api/Manga/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutManga(string id, Manga manga)
+        {
+            if (id != manga.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(manga).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MangaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Manga
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Manga>> PostManga(Manga manga)
+        {
+            _context.Manga.Add(manga);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (MangaExists(manga.id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetManga", new { id = manga.id }, manga);
+        }
+
+        // DELETE: api/Manga/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteManga(string id)
+        {
+            var manga = await _context.Manga.FindAsync(id);
+            if (manga == null)
+            {
+                return NotFound();
+            }
+
+            _context.Manga.Remove(manga);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MangaExists(string id)
+        {
+            return _context.Manga.Any(e => e.id == id);
+        }
+    }
+}
---
+++ backend/Models/SeedData.cs
@@ -0,0 +1,63 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using backend.Data;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace backend.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ApplicationDbContext>>()))
+            {
+                // Look for any Mangas.
+                if (context.Manga.Any() && context.Chap.Any() && context.Detail.Any())
+                {
+                    return;
+                }
+                context.Chap.AddRange(
+                    new Chap
+                    {
+                        id = "00001",
+
+                        number = 97,
+                        ReleaseDate = DateTime.Now,
+                        views = 0,
+                        pageCount = 108,
+                        MangaId = "00001"
+                    }
+                );
+                context.Detail.AddRange(
+                    new Detail
+                    {
+                        MangaId = "00001",
+                        title = "Bắc Kiếm Giang Hồ-Legend of the Northern Blade",
+                        author = "",
+                        condition = false,
+                        genre = "Action-Adventure-Cultivation-Fantasy-Manhwa-Martial Arts",
+                        stars = "0-0-0-0-0",
+                        views = 0,
+                        follows = 0,
+                        summary = ""
+                    }
+                );
+                context.Manga.AddRange(
+                    new Manga
+                    {
+                        id = "00001",
+                        lastUpdate = DateTime.Now,
+                        detail = context.Detail.FirstOrDefault(detail => detail.MangaId.Equals("00001")),
+                        chaps = context.Chap.Where(chap => chap.MangaId.Equals("00001")).ToList()
+                    }
+                );
+                Console.WriteLine("asd");
+                context.SaveChanges();
+            }
+        }
+    }
+}

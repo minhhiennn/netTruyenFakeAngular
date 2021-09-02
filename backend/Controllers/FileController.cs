0005a49a8408562f586ba0e4d3a4cdcdf0bd7d7c
shegga9x <74436981+shegga9x@users.noreply.github.com>
2021-09-02 16:55:12 +0700

    update upload promax full

--- backend/Controllers/FileController.cs
+++ backend/Controllers/FileController.cs
@@ -6,6 +6,7 @@ using System.IO.Compression;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using backend.Data;
 using Microsoft.AspNetCore.Authorization;
@@ -46,45 +47,74 @@ namespace backend.Controllers
                     try
                     {
                         await file.CopyToAsync(stream); stream.Close();
-                        String idManga = file.FileName.Split("-")[0];
-                        if (!_context.Manga.Any(manga => manga.id.Equals(idManga))) { System.IO.File.Delete(filePath); return NotFound(); }
-                        int chapNumber = Int32.Parse(file.FileName.Split("-")[1].Split(".")[0]);
-                        string chapID = (Int32.Parse(_context.Chap.OrderByDescending(p => p.id).FirstOrDefault().id) + 1) + "";
-                        var chapExist = _context.Chap.Where(chap => chap.number == chapNumber).Where(chap => idManga.Equals(idManga));
-                        if (chapExist.Any())
+                        if (checkFormatOfUnzip(filePath))
                         {
-                            chapID = chapExist.First().id;
-                            DirectoryInfo di1 = Directory.CreateDirectory(pathFileRoot + "/storage/" + chapID);
-                            foreach (FileInfo fileOld in di1.GetFiles()) { fileOld.Delete(); }
-                            di1.Delete(true);
-                        }
-                        else
-                        {
-                            _context.Chap.Add(new Chap
+                            String idManga = file.FileName.Split("-")[0];
+                            if (!_context.Manga.Any(manga => manga.id.Equals(idManga))) { System.IO.File.Delete(filePath); return NotFound(); }
+                            int chapNumber = Int32.Parse(file.FileName.Split("-")[1].Split(".")[0]);
+                            string chapID = (Int32.Parse(_context.Chap.OrderByDescending(p => p.id).FirstOrDefault().id) + 1) + "";
+                            var chapExist = _context.Chap.Where(chap => chap.number == chapNumber).Where(chap => idManga.Equals(idManga));
+                            if (chapExist.Any())
                             {
-                                id = chapID,
-                                number = chapNumber,
-                                ReleaseDate = DateTime.Now,
-                                views = 0,
-                                pageCount = 0,
-                                MangaId = idManga
-                            });
+                                chapID = chapExist.First().id;
+                                DirectoryInfo di1 = Directory.CreateDirectory(pathFileRoot + "/storage/" + chapID);
+                                foreach (FileInfo fileOld in di1.GetFiles()) { fileOld.Delete(); }
+                                di1.Delete(true);
+                            }
+                            else
+                            {
+                                _context.Chap.Add(new Chap
+                                {
+                                    id = chapID,
+                                    number = chapNumber,
+                                    ReleaseDate = DateTime.Now,
+                                    views = 0,
+                                    pageCount = 0,
+                                    MangaId = idManga
+                                });
+                            }
+                            await _context.SaveChangesAsync();
+                            DirectoryInfo di = Directory.CreateDirectory(pathFileRoot + "/storage/" + chapID);
+
+                            await Task.Run(() => ZipFile.ExtractToDirectory(filePath, pathFileRoot + "/storage/" + chapID));
+                            System.IO.File.Delete(filePath);
+                            DirectoryInfo d = new DirectoryInfo("wwwroot/storage/" + chapID);
+                            _context.Chap.Find(chapID).pageCount = d.GetFiles("*.jpg").Length;
+                            await _context.SaveChangesAsync();
                         }
-                        await _context.SaveChangesAsync();
-                        DirectoryInfo di = Directory.CreateDirectory(pathFileRoot + "/storage/" + chapID);
-                        await Task.Run(() => ZipFile.ExtractToDirectory(filePath, pathFileRoot + "/storage/" + chapID));
-                        System.IO.File.Delete(filePath);
-                        DirectoryInfo d = new DirectoryInfo("wwwroot/storage/" + chapID);                        
-                        _context.Chap.Find(chapID).pageCount = d.GetFiles("*.jpg").Length;
-                        await _context.SaveChangesAsync();
+                        else { System.IO.File.Delete(filePath); return Conflict(); }
+
                     }
-                    catch (System.Exception)
+                    catch (System.Exception e)
                     {
+                        System.Console.WriteLine(e);
                         return BadRequest();
                     }
                 }
             }
             return Ok();
         }
+
+        public Boolean checkFormatOfUnzip(string filePath)
+        {
+            ZipArchiveEntry prev = null;
+            bool first = true;
+            using (ZipArchive zip = ZipFile.Open(filePath, ZipArchiveMode.Read))
+                foreach (ZipArchiveEntry entry in zip.Entries)
+                {
+                    if (first) first = false;
+                    else
+                    {
+                        int file1Name = Int32.Parse(Regex.Replace(entry.Name, "[^0-9]+", "", RegexOptions.Compiled));
+                        int file2Name = Int32.Parse(Regex.Replace(prev.Name, "[^0-9]+", "", RegexOptions.Compiled));
+                        if (file2Name == zip.Entries.Count) return true;
+                        if (file1Name != (file2Name + 1)) return false;
+                    }
+                    prev = entry;
+
+                }
+            return true;
+        }
     }
+
 }
